46bc92d3f9e876dfaafbf736c53ebfc851a2135f
WilliBobadilla <willi1997.1@gmail.com>
2021-03-29 08:17:08 -0400

    hidden

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
                     level=logging.INFO)
 
 logger = logging.getLogger(__name__)
-TOKEN = '1760076810:AAGIu3VBXpCKCU_NYg_0B8gJU-FMrF6ZnGM'
+TOKEN = 'token'
 
 # Define a few command handlers. These usually take the two arguments update and
 # context. Error handlers also receive the raised TelegramError object in error.
